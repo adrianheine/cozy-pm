10eb0c9e4d6746b31b9961f8c4b0103c5ec67654
Adrian Heine <mail@adrianheine.de>
2018-04-25 14:28:18 +0200

    Reuse toDOM from schema in ExternalItemView

--- src/ExternalItemView.js
+++ src/ExternalItemView.js
@@ -1,9 +1,8 @@
+import {DOMSerializer} from "prosemirror-model"
+
 export class ExternalItemView {
   constructor(node, externalEntityTypes) {
-    this.dom = document.createElement("span")
-    this.dom.className = "cozy-external-item"
-    this.dom.dataset.cozyExternalId = node.attrs.id
-    this.dom.dataset.cozyExternalType = node.attrs.type
+    this.dom = DOMSerializer.renderSpec(window.document, node.type.spec.toDOM(node)).dom
     this.dom.textContent = "Loading …"
     this.unregister = externalEntityTypes[node.attrs.type].register(node.attrs, dom => this.dom.replaceChild(dom, this.dom.firstChild))
   }
